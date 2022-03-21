b61304a14587616a4d29ab491b4b20f8d47c0afb
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-20 20:31:56 -0600

    Fetch cats data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from "react";
 import "./App.scss";
+import { Route, Switch } from "react-router-dom";
 import catIcon from "./assets/icons/cat.png";
+import Cats from "./pages/Cats/Cats";
+// import CatsDetails from "./pages/CatsDetails/CatsDetails";
 
 function App() {
   return (
     <div>
-      <img src={catIcon} alt="" width="50" height="50" />
-      <p>meowdopt</p>
+      <div className="container">
+        <img src={catIcon} alt="" width="50" height="50" />
+        <p>meowdopt</p>
+
+        <Switch>
+          <Route path="/" exact component={Cats} />
+          {/* <Route path="/cats/:id" component={CatsDetails} /> */}
+          {/* <Route path="/shelter" component={Shelter} /> */}
+        </Switch>
+      </div>
     </div>
   );
 }
--- src/pages/Cats/Cats.js
+++ src/pages/Cats/Cats.js
@@ -1,1 +1,47 @@
 import "./Cats.scss";
+import { Component } from "react";
+import { Link } from "react-router-dom";
+
+import axios from "axios";
+
+const apiURL = `http://localhost:8080/cats`;
+class Cats extends Component {
+  state = {
+    cats: [],
+  };
+
+  componentDidMount() {
+    return this.fetchCats();
+  }
+
+  fetchCats = () => {
+    axios
+      .get(apiURL)
+      .then((response) => {
+        let cats = response.data;
+        console.log(cats);
+        this.setState({
+          cats: cats,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error trying to fetch the API.");
+      });
+  };
+  render() {
+    document.title = "Home | meowadopt";
+    return this.state.cats.map((item) => {
+      return (
+        <Link key={item.id} to="">
+          <img
+            src={`${process.env.REACT_APP_API_URL}${item.image}`}
+            alt="cat"
+          />
+          <p>{item.catName}</p>
+        </Link>
+      );
+    });
+  }
+}
+export default Cats;
