f01b0ceefa9ac38c6c0c6818ceaaf2bab6c33dd0
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-22 21:54:01 -0600

    Create CatsCard.js

---
+++ src/components/CatsCard/CatsCard.js
@@ -0,0 +1,18 @@
+import "./CatsCard.scss";
+import { Link } from "react-router-dom";
+import likeIcon from "./../../assets/icons/heart.svg";
+const baseURL = process.env.REACT_APP_API_URL;
+
+function CatsCard({ id, link, image, catName }) {
+  return (
+    <div className="cats__card">
+      <Link to={`cats/${id}`} className="cats__link">
+        <img src={likeIcon} alt="" arid-hidden="true" className="cats__like" />
+        <img src={`${baseURL}${image}`} alt="cat" className="cats__img" />
+        <p className="cats__name">{catName}</p>
+      </Link>
+    </div>
+  );
+}
+
+export default CatsCard;
