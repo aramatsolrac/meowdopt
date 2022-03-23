c9e1f6a530c5315a5a2eabd090e2f27425d18ded
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-22 21:53:56 -0600

    Add button function

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,1 +1,11 @@
 import "./Button.scss";
+
+function Button({ children, className, onClick }) {
+  return (
+    <button type="button" className={className} onClick={onClick}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
