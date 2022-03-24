336416608f2ec15bac472bcdd21fcbb84ea94961
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-24 13:46:49 -0600

    Add header

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import "./App.scss";
 import { Route, Switch } from "react-router-dom";
-import catIcon from "./assets/icons/cat.png";
 import Cats from "./pages/Cats/Cats";
 import CatDetails from "./pages/CatDetails/CatDetails";
 import Shelter from "./pages/Shelter/Shelter";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <div>
       <div className="container">
-        <img src={catIcon} alt="" width="50" height="50" />
-        <p>meowdopt</p>
-
+        <Header />
         <Switch>
           <Route path="/" exact component={Cats} />
           <Route path="/cats/:id" component={CatDetails} />
