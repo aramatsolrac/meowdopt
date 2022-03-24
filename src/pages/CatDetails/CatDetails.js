57214ce4a5f9b116e85d31e9a21b17ad1248d1a7
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-23 21:26:16 -0600

    Add adopt request and handle like

--- src/pages/CatDetails/CatDetails.js
+++ src/pages/CatDetails/CatDetails.js
@@ -2,15 +2,23 @@ import "./CatDetails.scss";
 import axios from "axios";
 import { Component } from "react";
 import { Link } from "react-router-dom";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import CatsCard from "../../components/CatsCard/CatsCard";
 import Button from "../../components/Button/Button";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 
 const baseURL = process.env.REACT_APP_API_URL;
 const catsURL = `${baseURL}/cats`;
+const requestURL = `${baseURL}/requests`;
 
 class CatDetails extends Component {
   state = {
     selectedCat: null,
+    isCatRequested: false,
+    isLiked: false,
   };
 
   componentDidMount() {
@@ -33,12 +41,123 @@ class CatDetails extends Component {
       });
   };
 
+  handleSubmit = (event) => {
+    event.preventDefault();
+    const MySwal = withReactContent(Swal);
+    const data = {
+      catID: this.state.selectedCat.id,
+      userID: "11",
+      name: "Test0",
+      email: "-",
+      status: "Received",
+    };
+    MySwal.fire({
+      title: (
+        <p>
+          Do you want to adopt{" "}
+          <span className="test">{this.state.selectedCat.catName}</span>?
+        </p>
+      ),
+      html: `<img src='${baseURL}${this.state.selectedCat.image}' alt="cat"/>`,
+      showCancelButton: true,
+      confirmButtonText: (
+        <div className="test2">
+          <FontAwesomeIcon icon={faHeart} />
+          <p>Yes</p>
+        </div>
+      ),
+      confirmButtonColor: "#dea48f",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axios
+          .post(`${requestURL}/${this.props.match.params.id}/form`, data)
+          .then((response) => {
+            console.log(response.data);
+            MySwal.fire({
+              position: "center",
+              icon: "success",
+              title: (
+                <div className="test3">
+                  <p>Submitted!</p>
+                  <p>
+                    <span className="test">
+                      {this.state.selectedCat.shelterName}{" "}
+                    </span>{" "}
+                    will contact you soon!
+                  </p>
+                  <p>
+                    {" "}
+                    <span className="test">
+                      {this.state.selectedCat.catName}
+                    </span>{" "}
+                    is looking forward to meet you!
+                  </p>
+                </div>
+              ),
+              showConfirmButton: true,
+              confirmButtonText: (
+                <div className="test2">
+                  <FontAwesomeIcon icon={faHeart} />
+                  <p>Ok</p>
+                </div>
+              ),
+              confirmButtonColor: "#dea48f",
+              timer: 15000,
+            });
+          })
+          .catch((error) => {
+            console.log(error);
+            alert("Error trying to fetch the API.");
+          });
+      }
+    });
+  };
+
+  handleLike = () => {
+    const data = {
+      catID: this.state.selectedCat.id,
+      userID: "11",
+    };
+    if (this.state.isLiked === false) {
+      axios
+        .post(`${catsURL}/${this.state.selectedCat.id}/like`, data)
+        .then((response) => {
+          this.setState({
+            isLiked: true,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Error trying to fetch the API.");
+        });
+    } else {
+      axios
+        .delete(`${catsURL}/${this.state.selectedCat.id}/remove-like`)
+        .then((response) => {
+          this.setState({
+            isLiked: false,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Error trying to fetch the API.");
+        });
+    }
+  };
+
+  handleBack = () => {
+    this.props.history.goBack();
+  };
   render() {
     document.title = `${
       this.state.selectedCat && this.state.selectedCat.catName
     } | meowadopt`;
+
+    const likeClass = this.state.isLiked === true ? "liked" : "";
+
     return (
       <>
+        <ArrowBackIcon onClick={this.handleBack} w={30} h={30} />
         {this.state.selectedCat && (
           <>
             <CatsCard
@@ -62,7 +181,17 @@ class CatDetails extends Component {
               <div>
                 <p>{this.state.selectedCat.description}</p>
               </div>
-              <Button children={"Adopt me"} />
+              <div>
+                <Button children="Adopt me" onClick={this.handleSubmit} />
+              </div>
+              <div>
+                <FontAwesomeIcon
+                  icon={faHeart}
+                  onClick={this.handleLike}
+                  className={`like ${likeClass}`}
+                  size="lg"
+                />
+              </div>
             </div>
           </>
         )}
