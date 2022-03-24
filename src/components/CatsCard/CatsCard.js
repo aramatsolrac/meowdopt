cc1c7649168540541d2e9d6c6b568fef6eeab316
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-23 21:25:34 -0600

    Remove like icon

--- src/components/CatsCard/CatsCard.js
+++ src/components/CatsCard/CatsCard.js
@@ -1,13 +1,11 @@
 import "./CatsCard.scss";
 import { Link } from "react-router-dom";
-import likeIcon from "./../../assets/icons/heart.svg";
 const baseURL = process.env.REACT_APP_API_URL;
 
-function CatsCard({ id, link, image, catName, urlPath }) {
+function CatsCard({ id, image, catName, urlPath }) {
   return (
     <div className="cats__card">
       <Link to={`${urlPath}/${id}`} className="cats__link">
-        <img src={likeIcon} alt="" arid-hidden="true" className="cats__like" />
         <img src={`${baseURL}${image}`} alt="cat" className="cats__img" />
         <p className="cats__name">{catName}</p>
       </Link>
