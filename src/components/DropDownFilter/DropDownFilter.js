b9c0e20a4bbc046a03c43d621b7ee9050c1767e8
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-26 18:47:23 -0600

    Add inputs and select

--- src/components/DropDownFilter/DropDownFilter.js
+++ src/components/DropDownFilter/DropDownFilter.js
@@ -2,10 +2,12 @@ import React, { Component } from "react";
 import "./DropDownFilter.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFilter } from "@fortawesome/free-solid-svg-icons";
+import Button from "./../Button/Button";
 
 class DropdownFilter extends Component {
   state = {
     dropdownVisible: false,
+    // filterInputs: "",
   };
 
   toggleDropdown = (e) => {
@@ -14,13 +16,70 @@ class DropdownFilter extends Component {
     }));
   };
 
+  // // function to filter cats
+  // filterCats = (event) => {
+  //   const filterInputs = event.target.value.toLowerCase();
+  //   console.log({ filterInputs });
+  //   const filteredCats = this.state.cats.filter((cat) => {
+  //     return (
+  //       cat.gender.toLowerCase().includes(filterInputs) &&
+  //       cat.age.toLowerCase().includes(filterInputs) &&
+  //       cat.city.toLowerCase().includes(filterInputs)
+  //     );
+  //   });
+  //   this.setState({
+  //     filteredCats: filteredCats,
+  //     filterInputs: filterInputs,
+  //   });
+  // };
+
   renderDropdownFilter = () => {
     return (
       <div className="dropdown__body">
-        <div className="dropdown__content">
-          <label htmlFor="male" />
+        <div className="dropdown__gender">
+          <h3>Gender</h3>
+          <label htmlFor="male">Male</label>
+          <input type="checkbox" value="Male" id="male" placeholder="Male" />
+          <label htmlFor="female">Female</label>
           <input type="checkbox" value="Male" id="male" placeholder="Male" />
         </div>
+        <div className="dropdown__age">
+          <h3>Age</h3>
+          <label htmlFor="kitty">Kitty</label>
+          <input type="checkbox" value="kitty" id="kitty" placeholder="Kitty" />
+          <label htmlFor="young">Female</label>
+          <input type="checkbox" value="Male" id="male" placeholder="Male" />
+          <label htmlFor="female">Young</label>
+          <input type="checkbox" value="young" id="young" placeholder="young" />
+          <label htmlFor="adult">Adult</label>
+          <input type="checkbox" value="adult" id="adult" placeholder="adult" />
+          <label htmlFor="senior">Senior</label>
+          <input
+            type="checkbox"
+            value="senior"
+            id="senior"
+            placeholder="senior"
+          />
+        </div>
+        <div className="dropdown__city">
+          <label htmlFor="city">City</label>
+          <select name="city" id="city">
+            <option hidden value="">
+              {""}
+            </option>
+            <option value="airdrie">Airdrie</option>
+            <option value="calgary">Calgary</option>
+            <option value="edmonton">Edmonton</option>
+            <option value="grandePrairie">Grande Prairie</option>
+            <option value="redDeer">Red Deer</option>
+            <option value="leduc">Leduc</option>
+            <option value="lethbridge">Lethbridge</option>
+            <option value="medicineHat">Medicine Hat</option>
+            <option value="saintAlbert">Saint Albert</option>
+            <option value="spruceGrove">Spruce Grove</option>
+          </select>
+        </div>
+        <Button children={"View Matches"} filterCats={this.props.filterCats} />
       </div>
     );
   };
