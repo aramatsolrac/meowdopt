56a5dd415f636142da2150cfa61384b11e5f0f88
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-26 18:48:36 -0600

    Add login router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Cats from "./pages/Cats/Cats";
 import CatDetails from "./pages/CatDetails/CatDetails";
 import Shelter from "./pages/Shelter/Shelter";
 import Header from "./components/Header/Header";
+import Login from "./pages/Login/Login";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" exact component={Cats} />
           <Route path="/cats/:id" component={CatDetails} />
           <Route path="/shelters/:id" component={Shelter} />
+          <Route path="/login" component={Login} />
         </Switch>
       </div>
     </div>
