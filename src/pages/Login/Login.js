2a6f4d31f70303f5311c980a599adb0c0f56fa67
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-26 18:49:15 -0600

    Add basic login form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,1 +1,44 @@
 import "./Login";
+import "./Login.scss";
+import { Component } from "react";
+import { login } from "../../helpers/authHelper";
+
+class Login extends Component {
+  //   state = {};
+
+  //   componentDidMount() {}
+
+  handleBack = () => {
+    this.props.history.goBack();
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    login(
+      event.target.userName.value,
+      event.target.password.value,
+      this.handleBack
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Username </label>
+            <input type="text" name="userName" required />
+          </div>
+          <div>
+            <label>Password </label>
+            <input type="password" name="password" required />
+          </div>
+          <div>
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default Login;
