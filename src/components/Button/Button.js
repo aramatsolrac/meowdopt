0abe17724cec440bd0969c3564e5c14ee54347db
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-27 21:31:47 -0600

    Change type to submit

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,7 +2,7 @@ import "./Button.scss";
 
 function Button({ children, className, onClick }) {
   return (
-    <button type="button" className={className} onClick={onClick}>
+    <button type="submit" className={className} onClick={onClick}>
       {children}
     </button>
   );
