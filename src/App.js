0dadbc5df3411bb76782909aa05fbbc15410def5
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-29 17:55:57 -0600

    Add favorites and requests routers

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ import Shelter from "./pages/Shelter/Shelter";
 import Header from "./components/Header/Header";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import Profile from "./pages/Profile/Profile";
+import FavoritesCats from "./pages/FavoritesCats/FavoritesCats";
+import RequestsCats from "./pages/RequestsCats/RequestsCats";
 
 function App() {
   return (
@@ -19,6 +22,9 @@ function App() {
           <Route path="/shelters/:id" component={Shelter} />
           <Route path="/login" component={Login} />
           <Route path="/signup" component={SignUp} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/:id/favorites" component={FavoritesCats} />
+          <Route path="/:id/requests" component={RequestsCats} />
         </Switch>
       </div>
     </div>
