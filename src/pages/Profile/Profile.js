ecc26f0e72625d14904987d5732a28197331c37f
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-29 21:42:20 -0600

    Add tabs

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,18 +1,55 @@
 import "./Profile.scss";
-import axios from "axios";
 import { Component } from "react";
-// import { isLoggedIn, getLoggedUser } from "../../helpers/authHelper";
-import Swal from "sweetalert2";
-import withReactContent from "sweetalert2-react-content";
-
-const baseURL = process.env.REACT_APP_API_URL;
-const usersURL = `${baseURL}/users`;
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart, faFile } from "@fortawesome/free-solid-svg-icons";
+import FavoritesCats from "../FavoritesCats/FavoritesCats";
+import RequestsCats from "../RequestsCats/RequestsCats";
 
 class Profile extends Component {
   state = {};
 
   render() {
-    return;
+    return (
+      <Tabs isFitted variant="enclosed">
+        <TabList mb="1em">
+          <Tab
+            _selected={{ color: "#dea48f", bg: "#fff2ed" }}
+            color="#dea48f"
+            _hover={{ bg: "#ffd6c8" }}
+            _focus={{ outlineColor: "#fabfab" }}
+          >
+            <FontAwesomeIcon
+              icon={faHeart}
+              className="profile__icon"
+              size="lg"
+            />
+            Favorites
+          </Tab>
+          <Tab
+            _selected={{ color: "#dea48f", bg: "#fff2ed" }}
+            color="#dea48f"
+            _hover={{ bg: "#ffd6c8" }}
+            _focus={{ outlineColor: "#fabfab" }}
+          >
+            <FontAwesomeIcon
+              icon={faFile}
+              className="profile__icon"
+              size="lg"
+            />
+            Requests
+          </Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel id="favorites">
+            <FavoritesCats />
+          </TabPanel>
+          <TabPanel id="requests">
+            <RequestsCats />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    );
   }
 }
 
