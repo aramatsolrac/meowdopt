447ad6cf13b2c70c5dab0568d02a7a94b4e2a1f3
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-29 21:42:05 -0600

    Add chakra ui

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
