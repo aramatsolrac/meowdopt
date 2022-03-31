83ebe59da42a3470d11e0a9e7c7381cae79b2ac2
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-30 21:12:23 -0600

    Create Footer.js

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import "./Footer.scss";
+
+function Footer() {
+  return (
+    <>
+      <footer className="footer">
+        <p className="footer__text">© meowadopt Inc. All Rights Reserved.</p>
+      </footer>
+    </>
+  );
+}
+export default Footer;
