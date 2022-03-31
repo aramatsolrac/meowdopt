3976fe7f9c52b0fbc68bc0faee9faa572b847e0a
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-30 21:42:15 -0600

    Add font-family

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
 import App from "./App";
 
+const theme = extendTheme({
+  fonts: {
+    heading: "Acumin Pro",
+    body: "Acumin Pro",
+  },
+});
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </Router>
