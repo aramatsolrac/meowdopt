346d3e138527a80a668593f7549956779d6f80a3
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-30 21:12:12 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ import CatDetails from "./pages/CatDetails/CatDetails";
 import Shelter from "./pages/Shelter/Shelter";
 import Header from "./components/Header/Header";
 import Login from "./pages/Login/Login";
-import SignUp from "./pages/SignUp/SignUp";
+// import SignUp from "./pages/SignUp/SignUp";
+// import SignIn from "./pages/SignIn/SignIn";
+
 import Profile from "./pages/Profile/Profile";
 import FavoritesCats from "./pages/FavoritesCats/FavoritesCats";
 import RequestsCats from "./pages/RequestsCats/RequestsCats";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
@@ -21,11 +24,13 @@ function App() {
           <Route path="/cats/:id" component={CatDetails} />
           <Route path="/shelters/:id" component={Shelter} />
           <Route path="/login" component={Login} />
-          <Route path="/signup" component={SignUp} />
+          {/* <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} /> */}
           <Route path="/profile" component={Profile} />
           <Route path="/:id/favorites" component={FavoritesCats} />
           <Route path="/:id/requests" component={RequestsCats} />
         </Switch>
+        <Footer />
       </div>
     </div>
   );
