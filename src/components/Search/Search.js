56516356af653f3ec75872c47f9be4e7c0f6abeb
Tamara Carlos <92123811+aramatsolrac@users.noreply.github.com>
2022-03-31 18:00:02 -0600

    Add wrapper

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -6,17 +6,19 @@ import DropdownFilter from "../DropDownFilter/DropDownFilter";
 const Search = ({ searchCats, filterCats, handleClear }) => {
   return (
     <div className="search">
-      <label htmlFor="search">
-        <FontAwesomeIcon icon={faSearch} className="search__icon-search" />
-      </label>
-      <input
-        className="search__field"
-        type="text"
-        id="search"
-        placeholder="Search"
-        onChange={searchCats}
-      ></input>
-      <DropdownFilter filterCats={filterCats} handleClear={handleClear} />
+      <div className="search__container">
+        <label htmlFor="search">
+          <FontAwesomeIcon icon={faSearch} className="search__icon-search" />
+        </label>
+        <input
+          className="search__field"
+          type="text"
+          id="search"
+          placeholder="Search"
+          onChange={searchCats}
+        ></input>
+        <DropdownFilter filterCats={filterCats} handleClear={handleClear} />
+      </div>
     </div>
   );
 };
